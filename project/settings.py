9d516c4fda859ff50fccef27faf89ee7f3dba439
PCopter <copter.phanuwat@gmail.com>
2025-02-04 00:14:34 +0700

    setting

--- project/settings.py
+++ project/settings.py
@@ -87,14 +87,30 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'mysql.connector.django',
+    #     'ENGINE': 'django.db.backends.mysql',  
+    #     'NAME': os.getenv("DB_NAME"),
+    #     'USER' : os.getenv("DB_USER"),
+    #     'PASSWORD' : os.getenv("DB_PASSWORD"),
+    #     'HOST' : os.getenv("DB_HOST", "localhost"),
+    #     'PORT' : os.getenv("DB_PORT", "3306"),
+    #     'OPTIONS': {
+    #         'charset': 'utf8mb4', 
+    #     },
+    # }
     'default': {
-        'ENGINE': 'mysql.connector.django',
-        'NAME': os.getenv("DB_NAME"),
-        'USER' : os.getenv("DB_USER"),
-        'PASSWORD' : os.getenv("DB_PASSWORD"),
-        'HOST' : os.getenv("DB_HOST"),
-        'PORT' : os.getenv("DB_PORT")
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mydb',
+        'USER': 'myuser',
+        'PASSWORD': 'mypassword',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+             'charset': 'utf8mb4', 
+         },
     }
+
 }
 
 
