2262f1e18aa6602dc2514b48eaf785650e2eba88
PCopter <copter.phanuwat@gmail.com>
2025-02-04 00:39:46 +0700

    อัปเดตฟีเจอร์ใหม่

--- project/settings.py
+++ project/settings.py
@@ -33,7 +33,7 @@ production_host = os.getenv("PRODUCTION_HOST")
 # ALLOWED_HOSTS = [production_host] if production_host is not None else ["127.0.0.1"]
 # ALLOWED_HOSTS = [production_host] if production_host is not None else ["127.0.0.1"]
 # ALLOWED_HOSTS = ["192.168.0.109" , "127.0.0.1" , "192.168.164.129"]
-ALLOWED_HOSTS = ["your-domain.com", "18.141.225.87"]
+ALLOWED_HOSTS = ["18.141.225.87"]
 
 
 # Application definition
