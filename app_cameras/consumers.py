60451b81334878908ef7a78f41e8043e0db62394
PCopter <copter.phanuwat@gmail.com>
2025-02-04 19:12:58 +0700

    pp

--- app_cameras/consumers.py
+++ app_cameras/consumers.py
@@ -1,5 +1,6 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from aiortc import RTCSessionDescription
 
 class WebRTCConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -11,8 +12,23 @@ class WebRTCConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         data = json.loads(text_data)
         if data['type'] == 'offer':
-            # รับ WebRTC Offer และส่ง Answer กลับไป
+            # รับ WebRTC Offer
+            offer_sdp = data['sdp']
+            offer = RTCSessionDescription(sdp=offer_sdp, type='offer')
+
+            # สร้าง Answer (ในกรณีนี้เราไม่ต้องการเสียง)
+            answer = await pc.createAnswer({
+                'offerToReceiveVideo': True,   # รับวิดีโอ
+                'offerToReceiveAudio': False,  # ไม่รับเสียง
+                'video': {
+                    'width': 1920,
+                    'height': 1080,
+                    'frameRate': 30
+                }
+            })
+
+            # ส่ง SDP Answer กลับไป
             await self.send(json.dumps({
                 'type': 'answer',
-                'sdp': 'Your WebRTC SDP Answer'
+                'sdp': answer.sdp
             }))
