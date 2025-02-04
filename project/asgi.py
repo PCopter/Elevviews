33117af991140c484758ca83da66b9854e197c72
PCopter <copter.phanuwat@gmail.com>
2025-02-04 18:23:12 +0700

    pp

--- project/asgi.py
+++ project/asgi.py
@@ -12,7 +12,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from app_cameras.routing import websocket_urlpatterns
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
