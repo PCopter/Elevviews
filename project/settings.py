547740133f05908d14727adcbab4db2c993fde00
PCopter <copter.phanuwat@gmail.com>
2025-02-05 22:58:32 +0700

    pp

--- project/settings.py
+++ project/settings.py
@@ -186,7 +186,7 @@ PASSWORD_RESET_TIMEOUT = 300
 
 import os
 # MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # AWS_Config Iot core
