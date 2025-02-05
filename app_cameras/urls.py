4737c354ee01b1dc3681fc3aad677a7635722be6
PCopter <copter.phanuwat@gmail.com>
2025-02-05 23:00:00 +0700

    pp

--- app_cameras/urls.py
+++ app_cameras/urls.py
@@ -8,4 +8,4 @@ urlpatterns = [
     path('<int:camera_id>', views.camera, name='camera'),
     path('<int:camera_id>/takephoto/', views.take_photo, name='takephoto'),
     path('api/upload_photo/', views.upload_photo, name='upload_photo'),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
